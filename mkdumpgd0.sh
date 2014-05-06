58dff307387bcb746b3c7f92426a7606d931a124
Tom Vijlbrief <tvijlbrief@gmail.com>
2014-05-06 20:08:16 +0200

    Added example dump script

---
+++ mkdumpgd0.sh
@@ -0,0 +1,12 @@
+PATH=.:$PATH
+
+rm /media/scratch/dump/*dump*
+
+dump -0 -f - /etc | gzip -c | split -b 100M - /media/scratch/dump/swan-etc.dump.gz.
+dump -0 -f - /home/tom/src | gzip -c | split -b 100M - /media/scratch/dump/swan-src.dump.gz.
+dump -0 -f - /home/tom/bin | gzip -c | split -b 100M - /media/scratch/dump/swan-bin.dump.gz.
+dump -0 -f - /home/tom/Desktop | gzip -c | split -b 100M - /media/scratch/dump/swan-desktop.dump.gz.
+
+bupload.sh /media/scratch/dump/*dump*
+
+echo Done: ; date
