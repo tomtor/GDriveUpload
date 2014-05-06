36c7986e179e8b1fb9e532525f8cb6dcd1d6cbd2
Tom Vijlbrief <tvijlbrief@gmail.com>
2014-05-06 21:14:28 +0200

    Add command line options

--- upload.py
+++ upload.py
@@ -3,13 +3,25 @@
 import sys
 import os
 
+from optparse import OptionParser
 from os.path import expanduser
-home = expanduser("~")
-cwd= os.getcwd()
 
 from pydrive.auth import GoogleAuth
 from pydrive.drive import GoogleDrive
 from pydrive.settings import LoadSettingsFile
+
+usage= "usage: %prog [options] FILE"
+parser = OptionParser(usage= usage)
+parser.add_option("-v", "--verbose", action="store_true", dest="verbose",
+                  help="verbose status messages", default=False)
+parser.add_option("-d", "--destination", dest="destination", metavar="DEST",
+                  help="destination directory (defaults to 'Dumps')",
+                  default="Dumps")
+
+(options, args) = parser.parse_args()
+
+home = expanduser("~")
+cwd= os.getcwd()
 
 home_upload= home + os.sep + ".upload.py" + os.sep
 
@@ -23,13 +35,16 @@ gauth.CommandLineAuth()
 drive = GoogleDrive(gauth)
 os.chdir(cwd)
 
-file_list = drive.ListFile({'q': "'root' in parents and title='Dumps'"}).GetList()
+file_list = drive.ListFile({'q': "title='" + options.destination + "'"}).GetList()
 
 parent = drive.CreateFile({'id': file_list[0]['id']})
 file1 = drive.CreateFile()
-file1['title']= sys.argv[1].replace(os.sep, '|')
+file1['title']= args[0].replace(os.sep, '|')
 file1['parents']= [parent]
-file1.SetContentFile(sys.argv[1])
-print "Start upload of " + file1['title']
+file1.SetContentFile(args[0])
+
+if options.verbose:
+  print "Start upload of " + file1['title'] + " to dir '" + options.destination + "'"
 file1.Upload()
-print "Done"
+if options.verbose:
+  print "Done"
